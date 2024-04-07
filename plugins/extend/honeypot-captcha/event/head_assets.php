b8cd63f167d64bed386417067070185e2a6cb853
jDanek <jirka.danek@jdanek.eu>
2024-04-07 14:21:24 +0200

    Minify css

--- plugins/extend/honeypot-captcha/event/head_assets.php
+++ plugins/extend/honeypot-captcha/event/head_assets.php
@@ -1,15 +1,5 @@
 <?php
 
 return function (array $args) {
-    $args['css_after'] .= '<style>.' . $this->getRandomCssClass() . ' {
-        clip: rect(0,0,0,0);
-        border: 0;
-        height: 1px;
-        margin: -1px;
-        overflow: hidden;
-        padding: 0;
-        position: absolute;
-        white-space: nowrap;
-        width: 1px;
-    }</style>';
+    $args['css_after'] .= "\n<style>." . $this->getRandomCssClass() . ' {clip: rect(0,0,0,0);border: 0;height: 1px;margin: -1px;overflow: hidden;padding: 0;position: absolute;white-space: nowrap;width: 1px;}</style>';
 };
