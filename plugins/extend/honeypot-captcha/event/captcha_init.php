734bb6137292d3496bc1e5052d34aa81e339de26
jDanek <jirka.danek@jdanek.eu>
2024-04-09 20:38:43 +0200

    Remove aria-hidden from inputs

--- plugins/extend/honeypot-captcha/event/captcha_init.php
+++ plugins/extend/honeypot-captcha/event/captcha_init.php
@@ -5,8 +5,8 @@ return function (array $args): void {
 
     $config = $this->getConfig();
 
-    $honeypotInputs = '<input type="text" name="' . _e($config['field1_name']) . '" value="" tabindex="-1" autocomplete="off" aria-hidden="true">'
-        . '<label><input type="checkbox" name="' . _e($config['field2_name']) . '" value="1" tabindex="-1" autocomplete="off" aria-hidden="true"> Very important</label>';
+    $honeypotInputs = '<input type="text" name="' . _e($config['field1_name']) . '" value="" tabindex="-1" autocomplete="off">'
+        . '<label><input type="checkbox" name="' . _e($config['field2_name']) . '" value="1" tabindex="-1" autocomplete="off"> Very important</label>';
 
     $args['value'] = [
         'label' => '<span aria-hidden="true">' . _lang('captcha.input') . '</span>',
