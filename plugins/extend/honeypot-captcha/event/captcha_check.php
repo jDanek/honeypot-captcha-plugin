759352019f7034e63d7bab82eee90900bb767a91
Shira <1024071+Shira-3749@users.noreply.github.com>
2024-05-08 14:49:57 +0200

    Log submitted data when JS token is invalid, exclude XSRF token

--- plugins/extend/honeypot-captcha/event/captcha_check.php
+++ plugins/extend/honeypot-captcha/event/captcha_check.php
@@ -1,6 +1,5 @@
 <?php
 
-use Sunlight\Logger;
 use Sunlight\Util\Request;
 
 return function (array $args) {
@@ -10,16 +9,7 @@ return function (array $args) {
         Request::post($config['field1_name']) !== ''
         || Request::post($config['field2_name']) !== null
     ) {
-        // remove passwords from log
-        $submitted_data = $_POST;
-        unset($submitted_data['password'], $submitted_data['password2']);
-
-        Logger::log(
-            $config['logger_level'],
-            'honeypot-captcha',
-            'The form was submitted with the "honeypot" field filled in',
-            ['submitted_data' => $submitted_data]
-        );
+        $this->logRequest('The form was submitted with the "honeypot" field filled in');
 
         // prevent further processing
         $args['value'] = false;
@@ -27,12 +17,7 @@ return function (array $args) {
     }
 
     if ($config['require_js'] && Request::post($config['js_field_name']) !== $this->getJsToken()) {
-        Logger::log(
-            $config['logger_level'],
-            'honeypot-captcha',
-            'The form was submitted without the correct JavaScript token field',
-            ['submitted_token' => Request::post($config['js_field_name'])]
-        );
+        $this->logRequest('The form was submitted without the correct JavaScript token field');
 
         // prevent further processing
         $args['value'] = false;
